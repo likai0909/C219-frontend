f4f51afaa2b9607e66a0090f9756eff5b5d21f73
Tan Li Kai <23011764@myrp.edu.sg>
2025-01-31 00:19:12 +0800

    Add files via upload

--- src/hooks/useWorkoutContext.js
+++ src/hooks/useWorkoutContext.js
@@ -1,15 +1,15 @@
 import { WorkoutContext } from "../context/WorkoutContext";
 import { useContext } from "react";
 
-
+// custom hook
 export const useWorkoutContext = () => {
-    const context = useContext(WorkoutContext); 
+  const context = useContext(WorkoutContext); // use the value passed in the provider
 
-    if (!context) {
-        throw Error(
-        "useWorkoutContext must be used inside a WorkoutContextProvider"
-        );
-    }
+  if (!context) {
+    throw Error(
+      "useWorkoutContext must be used inside a WorkoutContextProvider"
+    );
+  }
 
-    return context;
-    };
+  return context;
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,47 @@
 import { useEffect } from "react";
-import WorkoutDetails from "../component/WorkoutDetails";
-import WorkoutForm from "../component/WorkoutForm";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 import { useWorkoutContext } from "../hooks/useWorkoutContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 function Home() {
   // const [workouts, setWorkouts] = useState(null); // local state
   const { workouts, dispatch } = useWorkoutContext(); // global context state
+  const { user } = useAuthContext();
 
-    useEffect(() => {
-        const fetchWorkouts = async () => {
-        const response = await fetch(
-            `${process.env.REACT_APP_API_URL}/api/workouts`
-        );
-        const json = await response.json(); 
-        if (response.ok) {
-            
-            dispatch({ type: "SET_WORKOUTS", payload: json });
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/api/workouts`,
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
         }
-        };
+      );
+      const json = await response.json(); // parse JSON response body as JS array of objects
+      if (response.ok) {
+        // setWorkouts(workouts);
+        dispatch({ type: "SET_WORKOUTS", payload: json });
+      }
+    };
 
-        fetchWorkouts();
-    }, [dispatch]); 
-
-    return (
-        <div className="home">
-        <div className="workouts">
-            {workouts &&
-            workouts.map((workout) => (
-                <WorkoutDetails key={workout._id} workout={workout} />
-            ))}
-        </div>
-        <WorkoutForm />
-        </div>
-    );
+    if (user) {
+      fetchWorkouts();
     }
+  }, [dispatch, user]); // external function must be in dependency array
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+}
 
 export default Home;
